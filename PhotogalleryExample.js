2e87359a47bef5a27f27f0c416d71abd486f1906
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-08-31 17:18:30 +0530

    commit photo gallery

---
+++ PhotogalleryExample.js
@@ -0,0 +1,28 @@
+<!doctype html>
+<title>Example</title>
+<style>
+  .cards {
+    display: flex;
+    flex-wrap: wrap;
+    align-items: flex-start;
+    flex-direction: row;
+    max-height: 100vh;
+  }
+  .cards img {
+    margin: 10px;
+    border: 3px solid #000;
+    box-shadow: 3px 3px 8px 0px rgba(0,0,0,0.3); 
+    max-width: 23vw;
+  }
+</style>
+<main class="cards">
+  <img src="1.jpeg" alt="Sample photo">
+  <img src="2.jpeg" alt="Sample photo">
+  <img src="3.jpeg" alt="Sample photo">
+  <img src="4.jpeg" alt="Sample photo">
+  <img src="5.jpeg" alt="Sample photo">
+  <img src="6.jpeg" alt="Sample photo">
+  <img src="7.jpeg" alt="Sample photo">
+  <img src="8.jpeg" alt="Sample photo">
+  <img src="9.jpeg" alt="Sample photo">
+</main>
